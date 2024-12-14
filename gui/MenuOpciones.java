f47255b743f6fd33b46d8b312afb1595fdc8a509
fernandogerson <fluqueg@unsa.edu.pe>
2024-12-14 08:51:20 -0500

    Agregado la ventana para las opciones

---
+++ gui/MenuOpciones.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class MenuOpciones extends JFrame {
+    public MenuOpciones () {
+        setSize(600,480);
+
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+}

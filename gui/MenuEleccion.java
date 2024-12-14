d64120be3a29fb9c8505bdc10975375e85393640
fernandogerson <fluqueg@unsa.edu.pe>
2024-12-14 08:38:54 -0500

    Creando el menu de eleccion para jugar

---
+++ gui/MenuEleccion.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class MenuEleccion extends JFrame{
+    
+    public MenuEleccion() {
+        setSize(600,480);
+        setVisible(true);
+    }
+}
